04c709fc1c44f5a852abdc5c7e0ee39aed7dfbd9
Utkarsh Nandy <Utkarsh.nandy@gmail.com>
2024-04-01 23:21:05 -0700

    Added changes to intro.js

--- portfolio/components/intro.js
+++ portfolio/components/intro.js
@@ -1,42 +1,46 @@
-
-import styles from "./intro.module.css";
 import React, { useRef, useEffect } from 'react';
-
+import Image from 'next/image'; // Import Image component
+import styles from "./intro.module.css";
 
 const Intro = () => {
     const elementRef = useRef(null);
 
-  useEffect(() => {
-    if (elementRef.current) {
-      elementRef.current.style.transition = 'opacity 2s';
-      elementRef.current.style.opacity = 1;
-    }
-  }, []);
+    useEffect(() => {
+        if (elementRef.current) {
+            elementRef.current.style.transition = 'opacity 2s';
+            elementRef.current.style.opacity = 1;
+        }
+    }, []);
     
     return (
-        <header className={`${styles.intro}`}>
-                <div ref={elementRef} style={{opacity: 0}}>
-                    <div className={`${styles.person}`}>
+        <header className={styles.intro}>
+            <div ref={elementRef} style={{ opacity: 0 }}>
+                <div className={styles.person}>
                     🧑🏼‍💻
-                    </div>
-                <div className={`${styles.title}`}>
-                Hey, I'm Utkarsh
                 </div>
-                <div className={`${styles.subtitle}`}>
-                 and I'm studying Computer Science @ UC Davis!🐄.</div>
+                <div className={styles.title}>
+                    Hey, I&apos;m Utkarsh
+                </div>
+                <div className={styles.subtitle}>
+                    and I&apos;m studying Computer Science @ UC Davis! 🐄
+                </div>
                 <a href="https://www.linkedin.com/in/utkarsh-nandy-6286381b4/">
-                <img src="./icons8-linkedin.svg" alt="linkedin" className={`${styles.linkedin}`}></img>
+                    {/* Replace <img> with <Image> */}
+                    <Image src="/icons8-linkedin.svg" alt="linkedin" width={50} height={50} />
                 </a>
                 <a href="https://github.com/UtkarshNandy">
-                <img src="./icons8-github.svg" alt="github" className={`${styles.github}`}></img>
+                    {/* Replace <img> with <Image> */}
+                    <Image src="/icons8-github.svg" alt="github" width={50} height={50} />
                 </a>
-                <a href="mailto:utkarsh.nandy@gmail.com"> 
-                <img src="./icons8-mail-50.png" alt="mail" className={`${styles.mail}`}></img>
+                <a href="mailto:utkarsh.nandy@gmail.com">
+                    {/* Replace <img> with <Image> */}
+                    <Image src="/icons8-mail-50.png" alt="mail" width={50} height={50} />
                 </a>
-                <a href="./Utkarsh_Nandy_CS_Resume.pdf"> 
-                <img src="./icons8-resume-50.png" alt="resume" className={`${styles.resume}`}></img>
+                <a href="./Utkarsh_Nandy_CS_Resume.pdf">
+                    {/* Replace <img> with <Image> */}
+                    <Image src="/icons8-resume-50.png" alt="resume" width={50} height={50} />
                 </a>
-                </div>
+            </div>
         </header>
     );
 };
