2c84efb788117c91a7f1ccfa7f4b48b67bc35635
Utkarsh Nandy <Utkarsh.nandy@gmail.com>
2024-04-01 23:17:08 -0700

    changes to document.js

--- portfolio/pages/_document.js
+++ portfolio/pages/_document.js
@@ -1,26 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Utkarsh's Portfolio</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;500&display=swap" rel="stylesheet"/>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
-      <link rel="stylesheet" href="css/globals.css" />
-      
-      </Head> 
-      <body>
-        <div className ="container">
-        <Main />
-        </div>
-        <NextScript />
-      </body>
-      <footer>
-        Made by Utkarsh
-      </footer>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Utkarsh's Portfolio</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;500&display=swap" rel="stylesheet"/>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+          <link rel="stylesheet" href="/css/globals.css" />
+        </Head>
+        <body>
+          <div className="container">
+            <Main />
+          </div>
+          <NextScript />
+          <footer>
+            Made by Utkarsh
+          </footer>
+        </body>
+      </Html>
+    );
+  }
 }
